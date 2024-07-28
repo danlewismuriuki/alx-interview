8fa5017db84dfd41dca7a688f5bab51523e31bb8
Dan <danlewismuriuki@gmail.com>
2024-07-28 19:11:58 +0300

    Fix Pascal's Triangle implementation: correct row appending
    - Moved `res.append(next_row)` outside of the inner loop to ensure each row is only appended once.
    - Correctly computes and appends new rows to the Pascal's Triangle.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,20 +6,27 @@
 
 def pascal_triangle(n):
     """
-       Returns a list of list of intergers
-       Args:
-          n (int): size of the triangles
-       Returns: list
+        Returns a list of list of intergers
+        Args:
+           n (int): size of the triangles
+        Returns: list
     """
-    if n <= 0:
+
+    if (n <= 0):
         return []
 
     res = [[1]]
+
     for i in range(n - 1):
-        temp = [0] + res[-1] + [0]
-        row = []
-        for j in range(len(res[-1]) + 1):
-            row.append(temp[j] + temp[j + 1])
-        res.append(row)
+        # prev_row is the last row in the res array conating the results
+        prev_row = res[-1]
+        # Example prev_row - len 2 nex_row - 3
+        # Will fill the row with 1's example [1,1,1]
+        next_row = [1] * (len(prev_row) + 1)
+
+        # Add middle nums
+        for j in range(1, len(prev_row)):
+            next_row[j] = prev_row[j] + prev_row[j - 1]
+        res.append(next_row)
 
     return res
