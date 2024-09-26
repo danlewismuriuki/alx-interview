b4a5fa58b695c06c3d45555bc9085872943c6116
lsd <danlewismuriuki2@gmail.com>
2024-09-26 15:31:40 +0300

    fix: remove unnecessary blank line at end of 0-island_perimeter.py
    - Addressed W391 pycodestyle warning by removing the extra blank line.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,6 +4,7 @@
 Calculate the perimeter of an island in a grid
 """
 
+
 def island_perimeter(grid):
     perimeter = 0  # Initialize perimeter counter
 
@@ -16,19 +17,19 @@ def island_perimeter(grid):
                 perimeter += 4
 
                 # Check the cell above (i - 1)
-                if i > 0 and grid[i - 1][j] == 1:  
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 1
 
                 # Check the cell to the left (j - 1)
-                if j > 0 and grid[i][j - 1] == 1:  
+                if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 1
 
                 # Check the cell below (i + 1)
-                if i < rows - 1 and grid[i + 1][j] == 1:  
+                if i < rows - 1 and grid[i + 1][j] == 1:
                     perimeter -= 1
 
                 # Check the cell to the right (j + 1)
-                if j < columns - 1 and grid[i][j + 1] == 1:  
+                if j < columns - 1 and grid[i][j + 1] == 1:
                     perimeter -= 1
 
     return perimeter
