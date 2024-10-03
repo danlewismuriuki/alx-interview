9cb0ad8b546abe2e0169371baae4192748ba12f8
lsd <danlewismuriuki2@gmail.com>
2024-10-03 14:22:08 +0300

    Initialize prime_count array to track cumulative prime counts up to max_n
    - Created an array prime_count to store the count of primes up to each index.
    - This allows for quick access to the number of primes less than or equal to any number n.
    - The array is populated using a running total, where prime_count[i] stores the number of primes from 1 to i.

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,4 +1,4 @@
-def isWinnerx(x, nums):
+def isWinner(x, nums):
     max_n = max(nums)
 
     sieve = [True] * (max_n + 1)
@@ -8,3 +8,24 @@ def isWinnerx(x, nums):
         if sieve[i]:
             for multiple in range(i * i, max_n + 1, i):
                 sieve[multiple] = False
+
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if sieve[i] else 0)
+
+    maria_wins = 0
+    ben_wins = 0
+
+
+    for n in nums:
+        if prime_count[n] % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
